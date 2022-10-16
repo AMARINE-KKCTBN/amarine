f449abdcb242b0212a65ee9afcc52bf89b22a5ff
Robotiik Filkom <robotiik.filkom@.com>
2022-10-15 23:20:40 -0700

    adding testing program

--- controller/controller.py
+++ controller/controller.py
@@ -1,142 +1,142 @@
 import RPi.GPIO as GPIO
 from time import sleep
-from adafruit_servokit import ServoKit    #https://circuitpython.readthedocs.io/projects/servokit/en/latest/
+from adafruit_servokit import (
+    ServoKit,
+)  # https://circuitpython.readthedocs.io/projects/servokit/en/latest/
 
 
 class Controller:
-    def __init__(self, stepperPinConfig, thrusterPinConfig, mainThrusterPinConfig, allServoPinConfig) -> None:
+    def __init__(
+        self,
+        stepperPinConfig,
+        thrusterPinConfig,
+        mainThrusterPinConfig,
+        allServoPinConfig,
+    ) -> None:
         self.stepperPinConfig = stepperPinConfig
         self.thrusterPinConfig = thrusterPinConfig
         self.mainThrusterPinConfig = mainThrusterPinConfig
         self.allServoPinConfig = allServoPinConfig
-        pass
-
+        
     def setStepperConfiguration(self, step_sleep, step_count):
         self.step_count = step_count
         self.step_sleep = step_sleep
 
         # Set stepper pinout configuration
-        GPIO.setup( self.stepperPinConfig[0], GPIO.OUT )
-        GPIO.setup( self.stepperPinConfig[1], GPIO.OUT )
-        GPIO.setup( self.stepperPinConfig[2], GPIO.OUT )
-        GPIO.setup( self.stepperPinConfig[3], GPIO.OUT )
+        GPIO.setup(self.stepperPinConfig[0], GPIO.OUT)
+        GPIO.setup(self.stepperPinConfig[1], GPIO.OUT)
+        GPIO.setup(self.stepperPinConfig[2], GPIO.OUT)
+        GPIO.setup(self.stepperPinConfig[3], GPIO.OUT)
 
         # Init stepper pinout
-        GPIO.setup( self.stepperPinConfig[0], GPIO.LOW )
-        GPIO.setup( self.stepperPinConfig[1], GPIO.LOW )
-        GPIO.setup( self.stepperPinConfig[2], GPIO.LOW )
-        GPIO.setup( self.stepperPinConfig[3], GPIO.LOW )
-        
-        pass
+        GPIO.setup(self.stepperPinConfig[0], GPIO.LOW)
+        GPIO.setup(self.stepperPinConfig[1], GPIO.LOW)
+        GPIO.setup(self.stepperPinConfig[2], GPIO.LOW)
+        GPIO.setup(self.stepperPinConfig[3], GPIO.LOW)
 
     def forward_ballast(self):
         i = 0
         for i in range(self.step_count):
-            print("step: " + str(i) + "/" + str(self.step_count))
-            if i%4==0:
-                GPIO.output( self.stepperPinConfig[3], GPIO.HIGH )
-                GPIO.output( self.stepperPinConfig[2], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[1], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[0], GPIO.LOW )
-            elif i%4==1:
-                GPIO.output( self.stepperPinConfig[3], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[2], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[1], GPIO.HIGH )
-                GPIO.output( self.stepperPinConfig[0], GPIO.LOW )
-            elif i%4==2:
-                GPIO.output( self.stepperPinConfig[3], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[2], GPIO.HIGH )
-                GPIO.output( self.stepperPinConfig[1], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[0], GPIO.LOW )
-            elif i%4==3:
-                GPIO.output( self.stepperPinConfig[3], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[2], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[1], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[0], GPIO.HIGH )
-            sleep( 0.002 )
-        pass
-
+            print("FORWARD step: " + str(i) + "/" + str(self.step_count))
+            if i % 4 == 0:
+                GPIO.output(self.stepperPinConfig[3], GPIO.HIGH)
+                GPIO.output(self.stepperPinConfig[2], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[1], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[0], GPIO.LOW)
+            elif i % 4 == 1:
+                GPIO.output(self.stepperPinConfig[3], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[2], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[1], GPIO.HIGH)
+                GPIO.output(self.stepperPinConfig[0], GPIO.LOW)
+            elif i % 4 == 2:
+                GPIO.output(self.stepperPinConfig[3], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[2], GPIO.HIGH)
+                GPIO.output(self.stepperPinConfig[1], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[0], GPIO.LOW)
+            elif i % 4 == 3:
+                GPIO.output(self.stepperPinConfig[3], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[2], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[1], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[0], GPIO.HIGH)
+            sleep(0.002)
+        
     def backward_ballast(self):
         for i in range(self.step_count, 0, -1):
-            print("step: " + str(i) + "/" + str(self.step_count))
-            if i%4==0:
-                GPIO.output( self.stepperPinConfig[3], GPIO.HIGH )
-                GPIO.output( self.stepperPinConfig[2], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[1], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[0], GPIO.LOW )
-            elif i%4==1:
-                GPIO.output( self.stepperPinConfig[3], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[2], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[1], GPIO.HIGH )
-                GPIO.output( self.stepperPinConfig[0], GPIO.LOW )
-            elif i%4==2:
-                GPIO.output( self.stepperPinConfig[3], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[2], GPIO.HIGH )
-                GPIO.output( self.stepperPinConfig[1], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[0], GPIO.LOW )
-            elif i%4==3:
-                GPIO.output( self.stepperPinConfig[3], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[2], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[1], GPIO.LOW )
-                GPIO.output( self.stepperPinConfig[0], GPIO.HIGH )
-            sleep( 0.002 )
-        pass
-
+            print("BACKWARD step: " + str(i) + "/" + str(self.step_count))
+            if i % 4 == 0:
+                GPIO.output(self.stepperPinConfig[3], GPIO.HIGH)
+                GPIO.output(self.stepperPinConfig[2], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[1], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[0], GPIO.LOW)
+            elif i % 4 == 1:
+                GPIO.output(self.stepperPinConfig[3], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[2], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[1], GPIO.HIGH)
+                GPIO.output(self.stepperPinConfig[0], GPIO.LOW)
+            elif i % 4 == 2:
+                GPIO.output(self.stepperPinConfig[3], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[2], GPIO.HIGH)
+                GPIO.output(self.stepperPinConfig[1], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[0], GPIO.LOW)
+            elif i % 4 == 3:
+                GPIO.output(self.stepperPinConfig[3], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[2], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[1], GPIO.LOW)
+                GPIO.output(self.stepperPinConfig[0], GPIO.HIGH)
+            sleep(0.002)
+        
     def staticThruster(self, percentage):
+        percentage /= 100
         self.thrusterPinConfig[0].throttle = percentage
+        sleep(0.002)
         self.thrusterPinConfig[1].throttle = percentage
+        sleep(0.002)
         self.thrusterPinConfig[2].throttle = percentage
+        sleep(0.002)
         self.thrusterPinConfig[3].throttle = percentage
-        pass
+        sleep(0.002)
+        print("RUNNING 4 THRUSTER ON " + str(percentage))
+        sleep(0.002)
 
     def front(self):
-        self.allServoPinConfig[0].set_pulse_width_range(500 , 2500)
+        self.allServoPinConfig[0].set_pulse_width_range(500, 2500)
+        sleep(0.002)
         self.allServoPinConfig[0].angle = 90
-        pass
+        
 
     def back(self):
-        self.allServoPinConfig[0].set_pulse_width_range(500 , 2500)
-        self.allServoPinConfig[0].angle = 90 
-        pass
-
+        self.allServoPinConfig[0].set_pulse_width_range(500, 2500)
+        sleep(0.002)
+        self.allServoPinConfig[0].angle = 90
+        
     def left(self):
-        self.allServoPinConfig[0].set_pulse_width_range(500 , 2500)
+        self.allServoPinConfig[0].set_pulse_width_range(500, 2500)
+        sleep(0.002)
         self.allServoPinConfig[0].angle = 150
-        pass
-
+        
     def right(self):
-        self.allServoPinConfig[0].set_pulse_width_range(500 , 2500)
+        self.allServoPinConfig[0].set_pulse_width_range(500, 2500)
+        sleep(0.002)
         self.allServoPinConfig[0].angle = 30
-        pass
+        
+    def dynamicMove(self, angle):
+        self.allServoPinConfig[0].set_pulse_width_range(500, 2500)
+        self.allServoPinConfig[0].angle = angle
 
     def horizontalFinUp(self):
-        self.allServoPinConfig[1].set_pulse_width_range(500 , 2500)
+        self.allServoPinConfig[1].set_pulse_width_range(500, 2500)
+        sleep(0.002)
         self.allServoPinConfig[1].angle = 150
-        pass
-
+        
     def horizontalFinDown(self):
-        self.allServoPinConfig[1].set_pulse_width_range(500 , 2500)
+        self.allServoPinConfig[1].set_pulse_width_range(500, 2500)
+        sleep(0.002)
         self.allServoPinConfig[1].angle = 30
-        pass
+        
+    def cleanup(self):
+        GPIO.output(self.stepperPinConfig[3], GPIO.LOW)
+        GPIO.output(self.stepperPinConfig[2], GPIO.LOW)
+        GPIO.output(self.stepperPinConfig[1], GPIO.LOW)
+        GPIO.output(self.stepperPinConfig[0], GPIO.LOW)
+        GPIO.cleanup()
 
-
-if __name__ == "__main__":
-
-    pca = ServoKit(channels=16)
-
-    mainFin = pca.servo[0]
-    secondaryFin = pca.servo[1]
-
-    leftFrontProp = pca.continuous_servo[5]
-    leftBackProp = pca.continuous_servo[6]
-    rightFrontProp = pca.continuous_servo[7]
-    rightBackProp = pca.continuous_servo[8]
-
-    mainProp = pca.continuous_servo[4]
-
-    controller = Controller(
-        stepperPinConfig=[11,22,33,44], 
-        thrusterPinConfig=[leftBackProp, leftFrontProp, rightBackProp, rightFrontProp], 
-        mainThrusterPinConfig=mainProp, 
-        allServoPinConfig=[mainFin, secondaryFin]
-        )
--- main.py
+++ main.py
@@ -1,41 +1,73 @@
 
+import threading
 import RPi.GPIO as GPIO
-import json
-from adafruit_servokit import ServoKit    #https://circuitpython.readthedocs.io/projects/servokit/en/latest/
+from adafruit_servokit import ServoKit    
+from controller import controller as cntrl
 
-GPIO.setmode(GPIO.BOARD)
+def ballastButton(cnt): 
+    global ballast_status
+    while True:
+        if GPIO.input(ballast_button) == GPIO.HIGH:
+            if ballast_status == 'empty':
+                ballast_status = 'fill'
+                cnt.forward_ballast()
+            else:
+                ballast_status = 'empty'
+                cnt.backward_ballast()
 
-pca = ServoKit(channels=16)
-
-# PROPELLER CONSTRAINT
-leftFrontProp = pca.continuous_servo[0]
-leftBackProp = pca.continuous_servo[1]
-rightFrontProp = pca.continuous_servo[2]
-rightBackProp = pca.continuous_servo[3]
-mainProp = pca.continuous_servo[4]
-
-# SERVO CONSTRAINT
-mainFin = pca.servo[5]
-launcerGate = pca.servo[6]
-
-# STEPPER CONSTRAINT
-out1 = 16
-out2 = 19
-out3 = 20
-out4 = 26
-
-# SETTING UP STEPPER PINOUT
-GPIO.setup( out1, GPIO.OUT )
-GPIO.setup( out2, GPIO.OUT )
-GPIO.setup( out3, GPIO.OUT )
-GPIO.setup( out4, GPIO.OUT )
-
-# INIT STEPPER PINOUT
-GPIO.output( out1, GPIO.LOW )
-GPIO.output( out2, GPIO.LOW )
-GPIO.output( out3, GPIO.LOW )
-GPIO.output( out4, GPIO.LOW )
+def runThruster(cnt, percentage):
+    global ballast_status
+    while True:
+        print(ballast_status)
+        cnt.staticThruster(percentage)
 
 
 if __name__ == "__main__":
-    print("SS")
+
+    ballast_status = 'empty'
+    ballast_button = 18
+    GPIO.setup(ballast_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    step_sleep = 0.002
+
+    step_count = 2400 
+
+    percentage = 80 
+
+    pca = ServoKit(channels=16)
+
+    mainFin = pca.servo[0]
+    secondaryFin = pca.servo[1]
+
+    leftFrontProp = pca.continuous_servo[5]
+    leftBackProp = pca.continuous_servo[6]
+    rightFrontProp = pca.continuous_servo[7]
+    rightBackProp = pca.continuous_servo[8]
+
+    mainProp = pca.continuous_servo[4]
+
+    controller = cntrl.Controller(
+        stepperPinConfig=[11,22,33,44], 
+        thrusterPinConfig=[leftBackProp, leftFrontProp, rightBackProp, rightFrontProp], 
+        mainThrusterPinConfig=mainProp, 
+        allServoPinConfig=[mainFin, secondaryFin]
+        )
+
+    controller.setStepperConfiguration(step_sleep, step_count)
+
+    t1 = threading.Thread(target=ballastButton, args=(controller, ))
+    t2 = threading.Thread(target=runThruster, args=(controller, percentage))
+
+	# starting thread 1
+    t1.start()
+    t2.start()
+
+    # print(t1.is_alive())
+    # print(t2.is_alive())
+
+
+	# # wait until thread 1 is completely executed
+    # t1.join()
+    # t2.join()
+
+
