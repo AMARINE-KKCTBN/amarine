de452d02ac034fb37e7c6aee8d4454c1ff2b4929
Abdan Hurmuzi <abdan.idza2345@gmail.com>
2022-11-02 11:50:33 +0700

    update main.py

--- main.py
+++ main.py
@@ -31,21 +31,28 @@ def runMainThruster(cnt, serial):
     while True:
         data = serial.readline().decode('utf-8')
         print("DATA: ", data)
-        if count < 5:
-            print("COUNT KURANG DARI 5")
         if serial.in_waiting > 0:
-            if count < 5 and data == "0\r\n":
-                print("INIT...", count)
+            if data == "1\r\n":
+                print("RUNNING THRUSTER")
+                cnt.mainThruster(main_thruster_speed)
             else:
-                print("COUNT...", count)
-                if data == "1\r\n":
-                    print("RUNNING THRUSTER")
-                    cnt.mainThruster(main_thruster_speed)
-                else:
-                    print("STOP THRUSTER")
-                    cnt.mainThruster(0)
-                count=5
-            count+=1
+                print("STOP THRUSTER")
+                cnt.mainThruster(0)
+        # if count < 5:
+        #     print("COUNT KURANG DARI 5")
+        # if serial.in_waiting > 0:
+        #     if count < 5 and data == "0\r\n":
+        #         print("INIT...", count)
+        #     else:
+        #         print("COUNT...", count)
+        #         if data == "1\r\n":
+        #             print("RUNNING THRUSTER")
+        #             cnt.mainThruster(main_thruster_speed)
+        #         else:
+        #             print("STOP THRUSTER")
+        #             cnt.mainThruster(0)
+        #         count=5
+        #     count+=1
             sleep(0.1)
 
 def runMissile():
