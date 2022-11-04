88ac554f62d521f88bab6e8d6e086343ecd9a9ba
DrowsyLad <tsumwashere@student.ub.ac.id>
2022-11-04 14:47:43 +0700

    Add vertical limiter and fixed hsv filter

---
+++ test_vision_lib.py
@@ -0,0 +1,17 @@
+import vision.vision_lib as vision_lib
+
+offset_x = 0.5
+vision = vision_lib.hsv_detector()
+# vision.visualize()
+# vision.enable_limiter()
+while True:
+    vision.main_process()
+    coord_x, coord_y, coord_z = vision.get_circle_coord()
+    if coord_x < 0:
+        coord_x = -1
+    else:
+        coord_x -= offset_x
+    print("coord: " + str(vision.get_circle_coord()) + " | output: " + str(coord_x))
+        
+    if not vision.show_image():
+       break 
