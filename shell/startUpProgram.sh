8defa7696b5df1ba8645429ffc0e7d4a064e26b5
abdan <robotiik.filkom@.com>
2022-11-05 19:13:05 -0700

    last program ready to test

--- shell/startUpProgram.sh
+++ shell/startUpProgram.sh
@@ -4,5 +4,5 @@ HOME=/home/pi/KKCTBN-2022
 PYTHONPATH=/usr/bin/python3.9
 . /home/pi/KKCTBN-2022/bin/activate
 cd /home/pi/KKCTBN-2022
-python test_component/test_serial.py 
+python3 main.py
 echo SUCCESS RUNNING MAIN.PY IN BACKGROUND
